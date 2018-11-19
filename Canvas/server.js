4d423280f29835d8b1a2b3a239a9c482006b459c
Victor Nery <victornery.t@gmail.com>
2018-11-19 13:02:15 -0300

    Adicionando projetos

--- Canvas/server.js
+++ Canvas/server.js
@@ -10,3 +10,11 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
+
+app.get('/filling', (req, res) => {
+  res.sendFile(path.join(__dirname + '/FillingAText.html'))
+})
+
+app.get('/img', (req, res) => {
+  res.sendFile(path.join(__dirname + '/images.html'))
+})
