a037eafe621858b5960b2a1e044dd8ef4af9dcd4
Victor Nery <victornery.t@gmail.com>
2018-11-19 09:40:26 -0300

    Adicionando servidor node

---
+++ Canvas/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+var path = require('path');
+const port = 3000
+const app = express()
+
+app.listen(port, () => {
+  console.log(`Servidor está rodando na porta ${port}`)
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
